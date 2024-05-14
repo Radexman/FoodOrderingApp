2e3bd5bfa7c9ae4acbf379cebd2064362792830f
Radexman <Borderlandsmaniak@gmail.com>
2024-05-15 01:08:45 +0200

    Single Product Styling

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,15 +1,42 @@
 import { Stack, useLocalSearchParams } from 'expo-router';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import products from '@/assets/data/products';
 
 const ProductDetailsScreen = () => {
 	const { id } = useLocalSearchParams();
+	const product = products.find((p) => p.id.toString() === id);
+
+	if (!product) {
+		return <Text>Product not found</Text>;
+	}
 
 	return (
-		<View>
-			<Stack.Screen options={{ title: 'Details: ' + id }} />
-			<Text>Product Details Screen for id: {id}</Text>
+		<View style={styles.container}>
+			<Stack.Screen options={{ title: product.name }} />
+			<Image
+				source={{ uri: product.image }}
+				resizeMode='contain'
+				style={styles.image}
+			/>
+			<Text style={styles.price}>{`Price: $${product.price}`}</Text>
 		</View>
 	);
 };
 
 export default ProductDetailsScreen;
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: 'white',
+		flex: 1,
+		padding: 10,
+	},
+	image: {
+		width: '100%',
+		aspectRatio: 1,
+	},
+	price: {
+		fontSize: 18,
+		fontWeight: 'bold',
+	},
+});
