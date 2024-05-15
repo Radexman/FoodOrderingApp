7cf87fb339abd8c2442a2571c7797c8639d9c582
Radexman <Borderlandsmaniak@gmail.com>
2024-05-15 16:04:41 +0200

    Product Details

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import { Stack, useLocalSearchParams } from 'expo-router';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native';
+import Button from '@/src/components/Button';
 import products from '@/assets/data/products';
 
+const sizes = ['S', 'M', 'L', 'XL'];
+
 const ProductDetailsScreen = () => {
+	const [selectedSize, setSelectedSize] = useState('M');
 	const { id } = useLocalSearchParams();
 	const product = products.find((p) => p.id.toString() === id);
+
+	const addToCart = () => {
+		console.warn('Product added, size', selectedSize);
+	};
 
 	if (!product) {
 		return <Text>Product not found</Text>;
@@ -18,7 +27,27 @@ const ProductDetailsScreen = () => {
 				resizeMode='contain'
 				style={styles.image}
 			/>
-			<Text style={styles.price}>{`Price: $${product.price}`}</Text>
+			<Text style={styles.selectSizeText}>Select size</Text>
+			<View style={styles.sizes}>
+				{sizes.map((size, index) => (
+					<Pressable
+						onPress={() => {
+							setSelectedSize(size);
+						}}
+						key={index}
+						style={[styles.size, { backgroundColor: selectedSize === size ? 'gainsboro' : 'white' }]}
+					>
+						<Text style={[styles.sizeText, { color: selectedSize === size ? 'black' : 'lightgray' }]}>
+							{size}
+						</Text>
+					</Pressable>
+				))}
+			</View>
+			<Text style={styles.price}>{`Price: $${product.price.toFixed(2)}`}</Text>
+			<Button
+				onPress={addToCart}
+				text='Add to cart'
+			/>
 		</View>
 	);
 };
@@ -36,7 +65,29 @@ const styles = StyleSheet.create({
 		aspectRatio: 1,
 	},
 	price: {
-		fontSize: 18,
+		fontSize: 20,
 		fontWeight: 'bold',
+		marginTop: 'auto',
+	},
+	sizes: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		marginVertical: 10,
+	},
+	size: {
+		backgroundColor: 'gainsboro',
+		width: 50,
+		aspectRatio: 1,
+		borderRadius: 25,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	sizeText: {
+		fontSize: 20,
+		fontWeight: '500',
+	},
+	selectSizeText: {
+		fontSize: 15,
+		fontWeight: '500',
 	},
 });
